d05110477e44ca26a8a79cf82dde03820ad13080
rudraa097 <rudraa097@outlook.com>
2026-04-21 23:46:10 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-// Import your pages (only non-map ones)
-import Home from "./pages/Home";
+// Import your pages
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import NotFound from "./pages/NotFound";
@@ -10,7 +9,9 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        {/* Default route */}
+        <Route path="/" element={<Login />} />
+
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
 
